bb17f6323113f09ee83a3cc7e48b0ce116b9f7b1
Profiidev <benjamin@familiebuerger.com>
2023-09-17 20:43:51 +0200

    Initial Commit

---
+++ src/lib/components/MinecraftIcon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import NaturoIcon from '$lib/assets/MinecraftIcon.png';
+</script>
+
+<div>
+  <img src={NaturoIcon} alt="">
+</div>
+
+<style>
+  img{
+    width: 80%;
+    height: 80%;
+    padding: 10%;
+    border-radius: 50%;
+		transition-duration: 200ms;
+  }
+  img:hover{
+    border-radius: 30%;
+    transition-duration: 200ms;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import MinecraftIcon from '$lib/components/MinecraftIcon.svelte';
+	import MusicIcon from '$lib/components/MusicIcon.svelte';
+	import NaturoIcon from '$lib/components/NaturoIcon.svelte';
+</script>
+
+<div class="navbar-back">
+	<ul class="navbar">
+		<li>
+			<NaturoIcon/>
+		</li>
+		<li>
+			<MinecraftIcon/>
+		</li>
+		<li>
+			<MusicIcon/>
+		</li>
+	</ul>
+</div>
+<div class="content">
+	<slot />
+</div>
+
+<style>
+	:global(:root) {
+		--navbar-color: rgb(30, 31, 34);
+		--navbar-width: 70px;
+
+		--content-color: rgb(49, 51, 54);
+	}
+
+	.navbar-back {
+		height: 100vh;
+		width: var(--navbar-width);
+		background-color: var(--navbar-color);
+	}
+	.navbar {
+		margin: 0;
+		padding: 0;
+		list-style: none;
+	}
+	li {
+		color: rgb(180, 180, 180);
+		width: var(--navbar-width);
+		height: var(--navbar-width);
+	}
+
+	.content {
+		position: absolute;
+		left: var(--navbar-width);
+		top: 0;
+		height: 100vh;
+		width: calc(100vw - var(--navbar-width));
+		background-color: var(--content-color);
+	}
+</style>
