0246a4d42b9dd3c96f1bf9d387cb40be71c2b388
Profiidev <benjamin@familiebuerger.com>
2023-09-18 20:26:51 +0200

    Improved Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import LoginIcon from '$lib/components/LoginIcon.svelte';
 	import MinecraftIcon from '$lib/components/MinecraftIcon.svelte';
 	import MusicIcon from '$lib/components/MusicIcon.svelte';
 	import NaturoIcon from '$lib/components/NaturoIcon.svelte';
@@ -7,13 +8,28 @@
 <div class="navbar-back">
 	<ul class="navbar">
 		<li>
-			<NaturoIcon/>
+			<a href="/">
+				<NaturoIcon/>
+			</a>
+			<div class="tooltip">Dashboard</div>
 		</li>
 		<li>
-			<MinecraftIcon/>
+			<a href="/minecraftserver">
+				<MinecraftIcon/>
+			</a>
+			<div class="tooltip">Minecraft Server</div>
 		</li>
 		<li>
-			<MusicIcon/>
+			<a href="/musicbot">
+				<MusicIcon/>
+			</a>
+			<div class="tooltip">Music Bot</div>
+		</li>
+		<li class="login">
+			<a href="/login">
+				<LoginIcon/>
+			</a>
+			<div class="tooltip">Log in</div>
 		</li>
 	</ul>
 </div>
@@ -35,14 +51,55 @@
 		background-color: var(--navbar-color);
 	}
 	.navbar {
+		height: 100vh;
 		margin: 0;
 		padding: 0;
 		list-style: none;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
 	}
 	li {
 		color: rgb(180, 180, 180);
 		width: var(--navbar-width);
 		height: var(--navbar-width);
+		user-select: none;
+	}
+	.login{
+		margin-top: auto;
+	}
+	li .tooltip{
+		color: white;
+		white-space: nowrap;
+		visibility: hidden;
+		position: relative;
+		left: 110%;
+		top: -50%;
+		transform-origin: top left;
+		transform: scale(0) translate(0, -60%);
+		background-color: black;
+		display: inline-block;
+		border-radius: 7px;
+		padding: 8px;
+		z-index: 1;
+		transition-duration: 100ms;
+	}
+	li:hover .tooltip{
+		transform-origin: top left;
+		transform: scale(1) translate(0, -60%);
+		visibility: visible;
+		transition-duration: 100ms;
+	}
+	.tooltip::after{
+		content: "";
+		position: absolute;
+		top: 50%;
+		left: -5px;
+		transform: translate(0, -50%);
+		margin-left: -5px;
+		border-width: 5px;
+		border-style: solid;
+		border-color: transparent black transparent transparent;
 	}
 
 	.content {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,8 @@
 <h1>Gaming</h1>
+
+<style>
+  h1{
+    position: relative;
+    top: 200px;
+  }
+</style>
