97db26b61bb1a1e3c2c18c313fea0cb93510dd25
Benjamin Bürger <92174452+Profiidev@users.noreply.github.com>
2023-09-20 17:05:17 +0200

    Exec Test (#10)

--- src/lib/pocketbase/pocketbase.ts
+++ src/lib/pocketbase/pocketbase.ts
@@ -3,7 +3,9 @@ import { writable } from 'svelte/store';
 
 export const pb = new PocketBase('http://nacktebusen.de:8091');
 export const currentUser = writable(pb.authStore.model);
+export const token = writable(pb.authStore.token);
 
 pb.authStore.onChange((auth) => {
   currentUser.set(pb.authStore.model);
+  token.set(pb.authStore.token);
 });
