0c660f317eb31ca63db5d67323698cb87ebb9d1f
Benjamin Bürger <92174452+Profiidev@users.noreply.github.com>
2023-09-22 15:46:45 +0200

    Control_Icons (#21)

---
+++ git.bat
@@ -0,0 +1,9 @@
+@echo off
+:start
+set /p "name=Enter Name: "
+if not "%name%"=="%name: =%" goto start
+git.exe checkout -b %name%
+git.exe add .
+git.exe commit -m %name%
+git.exe push --set-upstream origin %name%
+git.exe checkout master
---
+++ src/lib/components/StopIcon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import StopIcon from '$lib/assets/StopIcon.png';
+</script>
+
+<div>
+  <img draggable="false" src={StopIcon} alt="">
+</div>
+
+<style>
+  img{
+    width: 60%;
+    height: 80%;
+    padding: 20%;
+  }
+</style>
